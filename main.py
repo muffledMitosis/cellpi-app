67c85e24b27d8143478c78cf40f2f14cbb032f99
Meth Munindradasa <methsndr@gmail.com>
2025-04-02 11:52:13 +0100

    Test sock func

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from pySIM7020.src.Device import Device
 from pySIM7020.src.TCP import TCP
+from pySIM7020.src.Socket import Socket
 
 device = Device()
 tcp = TCP(device)
 print(tcp.getDevice())
+print(device.getFirmwareVersion())
+
+sock = Socket()
+print(sock.getConnCommand())
